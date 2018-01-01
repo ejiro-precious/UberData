01f1c8181f7a0a734685f1526106dc6e79e041cb
cloudera <cloudera@quickstart.cloudera>
2018-01-01 08:16:36 -0800

    producer pull data into kafka in avro format

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Uber Streaming
 
+Project working with Spark Streaming, Kafka and avro.
+
+The first stage using spark streaming pull some message in avro format to kafka
+
 
 ## Kafka
 	spark-submit --driver-memory 1g --executor-memory 1g --class org.learn.kafka.producer.KafkaProducer UberSpark.jar uber_stream localhost:9092 100
--- src/main/scala/org/learn/kafka/producer/KafkaProducer.scala
+++ src/main/scala/org/learn/kafka/producer/KafkaProducer.scala
@@ -1,17 +1,23 @@
 package org.learn.kafka.producer
 
+import java.io.ByteArrayOutputStream
 import java.util.Properties
 
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.dstream.ConstantInputDStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.kafka.connect.json.JsonSerializer
 import org.apache.kafka.clients.producer.{ProducerConfig, RecordMetadata}
 import org.apache.spark.broadcast.Broadcast
 import org.codehaus.jackson.map.ObjectMapper
 import java.util.concurrent.Future
 
-import org.codehaus.jackson.JsonNode
+import org.apache.avro.Schema.Parser
+import org.apache.avro.Schema
+import org.apache.avro.generic.{GenericData, GenericRecord}
+import org.apache.avro.io.EncoderFactory
+import org.apache.avro.specific.SpecificDatumWriter
+
+import scala.io.Source
 
 case class Item(id: Int, value: Int) extends Serializable {
   override def toString: String = {
@@ -36,20 +42,35 @@ object KafkaProducer extends App {
     new StreamingContext(sparkConf, Seconds(2))
   }
 
+  //Read avro schema file and create generic record structure
+  val schema: Schema = new Parser().parse(Source.fromURL(getClass.getResource("/schema.avsc")).mkString)
+  val genericItem: GenericRecord = new GenericData.Record(schema)
+
+  // Create anonymous function
+  val itemByteArray = (item: GenericRecord) => {
+    val writer = new SpecificDatumWriter[GenericRecord](schema)
+    val out = new ByteArrayOutputStream()
+    val encoder = EncoderFactory.get.binaryEncoder(out, null)
+    writer.write(item, encoder)
+    encoder.flush()
+    out.close()
+
+    out.toByteArray
+  }
+
   // Setting kafka producer
-  val kafkaProducer: Broadcast[SparkKafkaProducer[String, String]] = {
-  //val kafkaProducer: Broadcast[SparkKafkaProducer[String, JsonNode]] = {
+  //val kafkaProducer: Broadcast[SparkKafkaProducer[String, String]] = {
+  val kafkaProducer: Broadcast[SparkKafkaProducer[String, Array[Byte]]] = {
 
     val properties = new Properties()
     properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap)
     properties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "uber_kafka")
     properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
-    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
-    //properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.connect.json.JsonSerializer")
+    //properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer")
 
-    //org.apache.kafka.connect.json.JsonSerializer
-    ssc.sparkContext.broadcast(SparkKafkaProducer[String, String](properties))
-    //ssc.sparkContext.broadcast(SparkKafkaProducer[String, JsonNode](properties))
+    //ssc.sparkContext.broadcast(SparkKafkaProducer[String, String](properties))
+    ssc.sparkContext.broadcast(SparkKafkaProducer[String, Array[Byte]](properties))
   }
 
   val items = (0 until messages.toInt).map(i => Item(i, i))
@@ -62,22 +83,23 @@ object KafkaProducer extends App {
   val objectMapper = new ObjectMapper()
   dStream.foreachRDD(rdd => {
 
-    println("####################################################")
-
     rdd.foreachPartition(partition => {
-
-      println("*************************************************************")
-
-      //val producer = new KafkaProducer[String, JsonSerializer](properties)
 
       val metadata: Stream[Future[RecordMetadata]] = partition.map{ record =>
 
-        //val jsonNode = objectMapper.valueToTree(record)
-        //val message = new ProducerRecord[String, JsonSerializer](topic, record.id.toString, jsonNode)
-        //  val message = new ProducerRecord[String, String](topic, record.id.toString, record.toString)
+        // val message = new ProducerRecord[String, String](topic, record.id.toString, record.toString)
 
-        kafkaProducer.value.send(topic, record.id.toString, record.toString)
-        //kafkaProducer.value.send(topic, record.id.toString, jsonNode)
+        // Create Record
+        genericItem.put("id", record.id)
+        genericItem.put("value", record.value)
+
+        // Serialize generic record to byte array
+        val serializedBytes = itemByteArray(genericItem)
+
+        println(s"****** Printing record: id: $record.id - value: $record.value")
+
+        kafkaProducer.value.send(topic, record.id.toString, serializedBytes)
+        //kafkaProducer.value.send(topic, record.id.toString, record.toString)
       }.toStream
 
       //metadata.foreach{ metadataRow =>
